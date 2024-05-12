98442e0d1876bb3edd657e580b584954519eeec7
priyanshu <priyanshu24pandey@gmail.com>
2024-05-12 16:40:35 +0530

    added env and readme

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
-
 export const getQueriedRepos = (query, sortValue) =>
-  axios.get(`${BASE_URL}/search/repositories?q=${query}&sort=${sortValue}`);
+  axios.get(
+    `${
+      import.meta.env.VITE_BASE_URL
+    }/search/repositories?q=${query}&sort=${sortValue}`
+  );
